8209c4946e40f44e43386517dac1489e14162c46
winmoh <mohamedaouinti789@gmail.com>
2024-02-29 12:11:00 +0100

    first commit

---
+++ src/main/java/com/microservices/demo/microservicejenkins/MicroserviceJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.microservicejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJenkinsApplication.class, args);
+	}
+
+}
