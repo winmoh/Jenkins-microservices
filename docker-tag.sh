4aa4f69e5a10fec8bb1668bd80c7b4fddd06f43b
winmoh <103373835+winmoh@users.noreply.github.com>
2024-03-04 08:45:10 +0000

    modifs

---
+++ docker-tag.sh
@@ -0,0 +1,4 @@
+docker tag product-service:latest maouinti/product-service:latest
+docker tag discovery-service:latest maouinti/discovery-service:latest
+docker tag gateway-service:latest maouinti/gateway-service:latest
+docker tag order-service:latest maouinti/order-service:latest
